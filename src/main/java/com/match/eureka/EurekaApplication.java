363b6c32f3a87633311b088c06ee4b0e7e30d9ff
zhangchao <jaguar_zc@sina.com>
2019-08-01 10:32:53 +0800

    1.提交第一个可用版本

---
+++ src/main/java/com/match/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.match.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author zhangchao
+ * @Date 2019/7/31 16:09
+ * @Version v1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
